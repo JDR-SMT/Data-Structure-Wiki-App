71db05ac646a6fb3280d8bf5b3f54680291f9334
JDR-SMT <30058666@tafe.wa.edu.au>
2023-08-21 13:41:56 +0800

    Final

--- DataStructureWikiApp/DataStructureWikiApp/MainWindow.xaml.cs
+++ DataStructureWikiApp/DataStructureWikiApp/MainWindow.xaml.cs
@@ -39,8 +39,10 @@ namespace DataStructureWikiApp
         #region Utility
         private void DisplayArray()
         {
+            // clears all items in ListViewOutput
             ListViewOutput.Items.Clear();
 
+            // adds elements 0 and 1 from each row in the 2D array to the appropriate ListViewOutput columns
             for (int row = 0; row < RowLength(); row++)
             {
                 List<Pop> pops = new List<Pop>
@@ -51,22 +53,26 @@ namespace DataStructureWikiApp
             }
         }
 
+        // returns the row length of the 2D array
         private int RowLength()
         {
             return DataStructures.GetLength(0);
         }
 
+        // returns the column length of the 2D array
         private int ColumnLength()
         {
             return DataStructures.GetLength(1);
         }
 
+        // clears text from and sets focus on TextBoxInput
         private void ClearFocus()
         {
             TextBoxInput.Clear();
             TextBoxInput.Focus();
         }
 
+        // clears text from TextBoxes Name, Category, Structure and Definition
         private void ClearAll()
         {
             TextBoxName.Clear();
@@ -75,6 +81,7 @@ namespace DataStructureWikiApp
             TextBoxDefinition.Clear();
         }
 
+        // displays a selected row from ListViewOutput in TextBoxes Name, Category, Structure and Definition
         private void ListViewOutput_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             try
@@ -91,27 +98,33 @@ namespace DataStructureWikiApp
             }
         }
 
+        // calls ClearAll and sets focus on TextBoxName
         private void TextBoxName_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-
+            ClearAll();
+            TextBoxName.Focus();
         }
         #endregion
 
         #region Add
         private void ButtonAdd_Click(object sender, RoutedEventArgs e)
         {
+            // if TextBoxes Name, Category, Structure and Definition is not empty
             if (!string.IsNullOrWhiteSpace(TextBoxName.Text) &&
                 !string.IsNullOrWhiteSpace(TextBoxCategory.Text) &&
                 !string.IsNullOrWhiteSpace(TextBoxStructure.Text) &&
                 !string.IsNullOrWhiteSpace(TextBoxDefinition.Text))
             {
+                // if ptr is less than 2D array row length
                 if (ptr < RowLength())
                 {
+                    // add TextBoxes Name, Category, Structure and Definition to ptr elements 0, 1, 2 and 3 in the 2D array
                     DataStructures[ptr, 0] = TextBoxName.Text;
                     DataStructures[ptr, 1] = TextBoxCategory.Text;
                     DataStructures[ptr, 2] = TextBoxStructure.Text;
                     DataStructures[ptr, 3] = TextBoxDefinition.Text;
 
+                    // increment ptr
                     ptr++;
 
                     ClearAll();
@@ -130,12 +143,14 @@ namespace DataStructureWikiApp
         #endregion
 
         #region Edit
+        // sets focus on TextBoxName
         private void ButtonEdit_Click(object sender, RoutedEventArgs e)
         {
             TextBoxName.Focus();
-            ButtonVisibility(false, true, false); 
+            ButtonVisibility(false, true, false); // show ButtonApply and ButtonCancel
         }
 
+        // replaces selected row in ListViewOutput with text from TextBoxName, TextBoxCategory, TextBoxStructure and TextBoxDefinition
         private void ButtonApply_Click(object sender, RoutedEventArgs e)
         {
             DataStructures[ListViewOutput.Items.IndexOf(ListViewOutput.SelectedItem), 0] = TextBoxName.Text;
@@ -145,20 +160,22 @@ namespace DataStructureWikiApp
 
             ClearAll();
             ClearFocus();
-            ButtonVisibility(true, false, true); 
+            ButtonVisibility(true, false, true); // hide ButtonApply and ButtonCancel
             BubbleSort();
         }
 
+        // clears selected row in ListViewOutput
         private void ButtonCancel_Click(object sender, RoutedEventArgs e)
         {
             ListViewOutput.SelectedItem = null;
             ClearAll();
             ClearFocus();
-            ButtonVisibility(true, false, true);
+            ButtonVisibility(true, false, true); // hide ButtonApply and ButtonCancel
         }
 
         private void ButtonVisibility(bool a, bool b, bool c)
         {
+            // if ButtonEdit is clicked
             if (a == false && b == true)
             {
                 ButtonEdit.Visibility = Visibility.Hidden;
@@ -166,6 +183,7 @@ namespace DataStructureWikiApp
                 ButtonCancel.Visibility = Visibility.Visible;
             }
 
+            // if ButtonApply or ButtonCancel is clicked
             if (a == true && b == false)
             {
                 ButtonEdit.Visibility = Visibility.Visible;
@@ -184,37 +202,138 @@ namespace DataStructureWikiApp
         #region Delete
         private void ButtonDelete_Click(object sender, RoutedEventArgs e)
         {
+            // display prompt
+            MessageBoxResult result = MessageBox.Show("Delete this data structure?", "", MessageBoxButton.YesNo);
 
+            // if yes, set elements in selected row from ListViewOutput as null
+            if (result == MessageBoxResult.Yes)
+            {
+                DataStructures[ListViewOutput.Items.IndexOf(ListViewOutput.SelectedItem), 0] = null;
+                DataStructures[ListViewOutput.Items.IndexOf(ListViewOutput.SelectedItem), 1] = null;
+                DataStructures[ListViewOutput.Items.IndexOf(ListViewOutput.SelectedItem), 2] = null;
+                DataStructures[ListViewOutput.Items.IndexOf(ListViewOutput.SelectedItem), 3] = null;
+
+                // decrease ptr
+                ptr--;
+
+                ClearAll();
+                BubbleSort();
+            }
         }
         #endregion
 
         #region Bubble Sort
         private void BubbleSort()
         {
-
+            for (int a = 0; a < RowLength(); a++)
+            {
+                for (int b = a + 1; b < RowLength(); b++)
+                {
+                    // if element a, 0 is after element b, 0 and neither is null, swap positions
+                    if (DataStructures[a, 0] != null && DataStructures[b, 0] != null &&
+                        string.Compare(DataStructures[a, 0], DataStructures[b, 0]) > 0)
+                    {
+                        Swap(a, b);
+                    }
+                    // if element a, 0 is null and element b, 0 is not null, swap positions
+                    else if (DataStructures[a, 0] == null && DataStructures[b, 0] != null)
+                    {
+                        Swap(a, b);
+                    }
+                    DisplayArray();
+                }
+            }
         }
 
         private void Swap(int a, int b)
         {
-
+            for (int col = 0; col < ColumnLength(); col++)
+            {
+                string temp = DataStructures[a, col];
+                DataStructures[a, col] = DataStructures[b, col];
+                DataStructures[b, col] = temp;
+            }
         }
         #endregion
 
         #region Binary Search
         private void ButtonBinarySearch_Click(object sender, RoutedEventArgs e)
         {
+            // sorts 2D array to ensure BinarySearch is functional
+            BubbleSort();
 
+            // TextBoxInput is not empty
+            if (!string.IsNullOrWhiteSpace(TextBoxInput.Text))
+            {
+                // returns index of TextBoxInput in 2D array
+                int index = BinarySearch(DataStructures, TextBoxInput.Text);
+
+                if (index >= 0) // found
+                {
+                    MessageBox.Show(TextBoxInput.Text + " found.");
+                    ListViewOutput.SelectedIndex = index;
+                    ClearFocus();
+                }
+                else // not found
+                {
+                    MessageBox.Show(TextBoxInput.Text + " not found.");
+                    ListViewOutput.SelectedIndex = -1;
+                    ClearFocus();
+                }
+            }
+            else // TextBoxInput is empty
+            {
+                MessageBox.Show("Please enter a word to search.");
+                ClearFocus();
+            }
         }
 
-        private void BinarySearch(string[,] arr, string target)
+        private int BinarySearch(string[,] arr, string target)
         {
+            List<string> list = new List<string>();
 
+            // add all elements a, 0 from 2D array that are not null to list
+            for (int a = 0; a < RowLength(); a++)
+            {
+                if (arr[a, 0] != null)
+                {
+                    list.Add(arr[a, 0]);
+                }
+            }
+            // add list to temp array
+            string[] temp = list.ToArray();
+
+            int min = 0;
+            int max = temp.Length - 1;
+
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+
+                // compare element in temp array to target with ignoreCase
+                int index = string.Compare(temp[mid], target, true);
+
+                if (index == 0) // target exists
+                {
+                    return mid;
+                }
+                else if (index > 0) // search right
+                {
+                    max = mid - 1;
+                }
+                else if (index < 0) // search left
+                {
+                    min = mid + 1;
+                }
+            }
+            return -1; // target does not exist
         }
         #endregion
 
         #region Open
         private void ButtonOpen_Click(object sender, RoutedEventArgs e)
         {
+            // displays prompt to open a data file
             OpenFileDialog ofd = new OpenFileDialog { Filter = "data files (*.dat)|*.dat" };
 
             // if ok
@@ -235,14 +354,18 @@ namespace DataStructureWikiApp
         {
             try
             {
+                // clears all elements in 2D array
                 Array.Clear(DataStructures);
 
                 int row = 0;
 
+                // reads file
                 using (BinaryReader br = new BinaryReader(new FileStream(file, FileMode.Open)))
                 {
+                    // while position does not equal length of BaseStream
                     while (br.BaseStream.Position != br.BaseStream.Length)
                     {
+                        // read and add string to row elements 0, 1, 2 and 3 in the 2D array
                         DataStructures[row, 0] = br.ReadString();
                         DataStructures[row, 1] = br.ReadString();
                         DataStructures[row, 2] = br.ReadString();
@@ -251,6 +374,8 @@ namespace DataStructureWikiApp
                         row++;
                     }
                 }
+
+                // set ptr as row
                 ptr = row;
             }
             catch (Exception ex) // when file cannot be read from
@@ -263,6 +388,7 @@ namespace DataStructureWikiApp
         #region Save
         private void ButtonSave_Click(object sender, RoutedEventArgs e)
         {
+            // displays prompt to save the 2D array to a data file
             SaveFileDialog sfd = new SaveFileDialog
             {
                 DefaultExt = "dat",
@@ -287,10 +413,13 @@ namespace DataStructureWikiApp
         {
             try
             {
+                // creates file
                 using (BinaryWriter bw = new BinaryWriter(new FileStream(file, FileMode.Create)))
                 {
+                    // while row is less than ptr (to not include nulls)
                     for (int row = 0; row < ptr; row++)
                     {
+                        // writes row elements 0, 1, 2 and 3 into the 2D array
                         bw.Write(DataStructures[row, 0]);
                         bw.Write(DataStructures[row, 1]);
                         bw.Write(DataStructures[row, 2]);

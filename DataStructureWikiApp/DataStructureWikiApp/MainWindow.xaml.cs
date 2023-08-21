ecf8edc0f67b171a524ffbdbb3386ec3fafe4e51
JDR-SMT <30058666@tafe.wa.edu.au>
2023-08-21 13:45:17 +0800

    Removing Unnecessary Usings

--- DataStructureWikiApp/DataStructureWikiApp/MainWindow.xaml.cs
+++ DataStructureWikiApp/DataStructureWikiApp/MainWindow.xaml.cs
@@ -1,24 +1,10 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Intrinsics.X86;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 
 namespace DataStructureWikiApp
 {
